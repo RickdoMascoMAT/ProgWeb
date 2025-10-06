046854cc3030b185b384950a62e45ed036729b43
RickdoMascoMAT <riccardo.mascotto@mat.tn.it>
2025-10-06 14:08:00 +0200

    4.0.1

---
+++ Lezioni/06.10.2025/script.js
@@ -0,0 +1,13 @@
+//Deprecated: *var* Temperatura = 32;
+let nome = "Tommaso";
+const valore = 3920;
+
+console.log("Nome: " + nome);
+console.log(`ciao ${nome}`);
+
+let temp = 34;
+let tp = temp;
+
+console.log(temp);
+console.log(tp);
+
